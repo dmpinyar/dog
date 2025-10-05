5d589eeec4adcc02f9c0e12d94011d6fafcf0d60
dmpinyar <dmpinyard@gmail.com>
2025-10-05 18:01:38 +0000

    added more parameters to appu script and provided documentation for all of them. Got systemd.service to work correctly, and now it starts a screen session running the server on boot. Other than maybe cleaning up some of these magic strings in another file I'm probably done with python scripts

--- com/apache_push_helper.py
+++ com/apache_push_helper.py
@@ -3,7 +3,23 @@ import sys
 import os
 
 # aliased by appu='python3 /home/devin/projects/dog/com/apache_push_helper.py'
-# use -g to build grok server after
+
+### SCRIPT INFO ###
+# desc:
+# helps push files from a permanent, simple to navigate location in the filetree to where
+# apache deals with them. Has a bunch of side parameters for running the server as well
+# parameters:
+# g to build grok server within the same terminal window
+# b to build grok server within a different screen session
+# r to terminate any active screen sessions related to this script
+# c to load into the screen
+# n to opt out of pushing the files
+
+# b, r, and c don't play well with g
+# n just doesn't do anything if no other parameters are selected
+
+# for screen parameters this is the default name for the screen
+screen_name = "ngrok"
 
 # useful for checking if a .pl file needs to be converted to unix formatting so that we can still read
 # actual error logs in the instance we just automatically convert every file and have to pipe stderr,
@@ -15,8 +31,10 @@ def needs_dos2unix(path):
             return True
     return False
 
-# just to be safe
-if __name__ == "__main__":
+# turned into a method because the parameters I was passing in started getting really weird
+def push_changes():
+    print("pushing changes from permanent location to apache filetree...")
+
     # generate backups from previous version
     # create backup directories
     subprocess.run(['mkdir', '-p', '/home/devin/projects/dog/prev_ver'])
@@ -45,18 +63,32 @@ if __name__ == "__main__":
 
     subprocess.run(['cp', '-r', '/home/devin/projects/dog/cgi-bin/.', '/usr/lib/cgi-bin/'])
 
+if __name__ == "__main__":
     if (len(sys.argv) > 1 and sys.argv[1][0] == '-'):
+        if (sys.argv[1].find('n') < 0):
+            push_changes()
         if (sys.argv[1][1] == 'g'):
             # port 80 is defaulted with apache server
             subprocess.run(['ngrok', 'http', '80'])
-        elif (sys.argv[1][1:3] == 'bg'):
-            screen_name = "ngrok"
-            command = "ngrok http 80"
+        else:
+            if (sys.argv[1].find('r') >= 0):
+                process = subprocess.run(['screen', '-X', '-S', 'ngrok', 'quit'], 
+                            capture_output=True, 
+                            text=True)
+                if (not process.stdout):
+                    print(f"closing ngrok on screen: {screen_name}...")
+            if (sys.argv[1].find('b') >= 0):
+                command = "ngrok http 80"
 
-            subprocess.run([
-                "screen",
-                "-dmS", screen_name,
-                "bash", "-c", command
-            ])
-            
-            print("running ngrok in the background on in screen ngrok on port 80")
+                subprocess.run([
+                    "screen",
+                    "-dmS", screen_name,
+                    "bash", "-c", command
+                ])
+                
+                print(f"forwarding apache server in the background on screen: {screen_name}...")
+            if (sys.argv[1].find('c') >= 0):
+                subprocess.run(["screen", "-r", screen_name])
+    else:
+        push_changes()
+        
--- com/website_boot.py
+++ com/website_boot.py
@@ -1,8 +1,3 @@
-#### dysfunctional. Ngrok can boot on its own in startup but it is a pain to manage without a screen. I disabled the service ####
-#### but I am going to leave this file in case I want to revisit a similar prospect in the future ####
-
-
-
 import subprocess
 
 # script ran on boot built in /etc/systemd/system/website_boot.service
@@ -10,14 +5,14 @@ import subprocess
 ### the contents of the file ###
 # [Unit]
 # Description=Run website_boot.py inside screen
-# After=network.target
+# After=network-online.target
+# Wants=network-online.target
 
 # [Service]
 # Type=forking
 # User=devin
 # WorkingDirectory=/home/devin
-# ExecStart=/usr/bin/screen -dmS website_boot /usr/bin/python3 /home/devin/projects/dog/com/website_boot.py
-# Restart=always
+# ExecStart=/usr/bin/python3 /home/devin/projects/dog/com/website_boot.py      Restart=always
 # RestartSec=10
 
 # [Install]
@@ -31,13 +26,13 @@ import subprocess
 ### run "sudo systemctl status sudo systemctl status website_boot.service.service" to verify ###
 
 # name of the screen session
-screen_name = "grok"
+screen_name = "ngrok"
 
 # Launch a detached screen session running the ngrok on boot
+
+command = "ngrok http 80"
 subprocess.run([
     "screen",
     "-dmS", screen_name,
-    "bash", "-c", "ngrok", "http", "80"
+    "bash", "-c", command
 ])
-
-print(f"Started ngrok in detached screen session '{screen_name}'")
