1c6093c7e208af354648885042f94af664f527b1
dmpinyar <dmpinyard@gmail.com>
2025-10-07 17:06:02 +0000

    finally resolved multiscreen issue caused by overlap between service file and appu script. Opted to just terminate service file execution because I didn't like it doing stuff after boot, but I may just put a conditional in there in the future or something

--- com/apache_push_helper.py
+++ com/apache_push_helper.py
@@ -11,18 +11,18 @@ Description:
 Parameters:
     g : Build the Grok server within the same terminal window.
     b : Build the Grok server within a separate screen session.
-    r : Terminate any active screen sessions related to this script.
     c : Attach to the screen session.
-    n : Skip pushing files (no-op if used alone).
+    n : Skip pushing files.
 
 Notes:
-    - Parameters b, r, and c do not work well with g.
-    - Parameter n does nothing if no other parameters are selected.
+    - By default the script kills all relevant screens sessions.
+    - Parameters b and c do not work well with g.
 """
 
 import subprocess
 import sys
 import os
+import re
 
 # for screen parameters this is the default name for the screen
 screen_name = "ngrok"
@@ -41,7 +41,7 @@ def needs_dos2unix(path):
 
     Returns:
         bool: True if the file contains Windows-style line endings and should
-        be converted with `dos2unix`; False otherwise.
+        be converted with 'dos2unix'; False otherwise.
     """
 
     with open(path, "rb") as file:
@@ -54,15 +54,16 @@ def attempt_screen_close():
     """
     Attempts to close any screens currently running the ngrok server
     """
+
     result = subprocess.run(['screen', '-ls'], capture_output=True, text=True)
-    if screen_name in result.stdout:
-        process = subprocess.run(['screen', '-X', '-S', screen_name, 'quit'], 
-                    capture_output=True, 
-                    text=True)
-        if (not process.stdout):
-            print(f"closing ngrok on screen: {screen_name}...")
-        else:
-            exit(f"error closing screen {screen_name}: {process.stdout}")
+
+    for line in result.stdout.splitlines():
+        match = re.match(rf"\s*(\d+)\.{re.escape(screen_name)}", line)
+        if match:
+            pid = match.group(1)
+            subprocess.run(['screen', '-X', '-S', pid, 'quit'])
+            print(f"closing ngrok on screen: {pid}...")
+    
 
 def push_changes():
     """
@@ -103,31 +104,26 @@ def push_changes():
 
 if __name__ == "__main__":
     # address all call parameters
+    attempt_screen_close()
     
     if (len(sys.argv) > 1 and sys.argv[1][0] == '-'):
         if (sys.argv[1].find('n') < 0):
             push_changes()
         if (sys.argv[1][1] == 'g'):
             # port 80 is defaulted with apache server
-            attempt_screen_close()
             subprocess.run(['ngrok', 'http', '80'])
         else:
-            if (sys.argv[1].find('r') >= 0):
-                attempt_screen_close()
-            else:
-                if (sys.argv[1].find('b') >= 0):
-                    attempt_screen_close()
-                    command = "ngrok http 80"
-
-                    subprocess.run([
-                        "screen",
-                        "-dmS", screen_name,
-                        "bash", "-c", command
-                    ])
+            if (sys.argv[1].find('b') >= 0):
+                command = "ngrok http 80"
+                subprocess.run([
+                    "screen",
+                    "-dmS", screen_name,
+                    "bash", "-c", command
+                ])
                     
-                    print(f"forwarding apache server in the background on screen: {screen_name}...")
-                if (sys.argv[1].find('c') >= 0):
-                    subprocess.run(["screen", "-r", screen_name])
+                print(f"forwarding apache server in the background on screen: {screen_name}...")
+            if (sys.argv[1].find('c') >= 0):
+                subprocess.run(["screen", "-r", screen_name])
     else:
         push_changes()
         
