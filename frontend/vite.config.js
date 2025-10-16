a110fb59f1f4998320f0d9ff121f73cea8f20819
dmpinyar <dmpinyard@gmail.com>
2025-10-16 02:04:49 +0000

    guess what I got it working on the server, I just haven't updated the current script

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "/var/www/html",
     emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
   },
 })
